fc5d8be1e13776b29cf36e277c4584f19362e846
BenchayaM <151805252+BenchayaM@users.noreply.github.com>
2025-05-14 21:39:26 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,14 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'flagcdn.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 }
 
