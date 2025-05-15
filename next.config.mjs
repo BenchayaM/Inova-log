cb422eb17d154db377e9a73a90702a2a0f9d736c
BenchayaM <151805252+BenchayaM@users.noreply.github.com>
2025-05-14 21:12:08 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,13 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Adicione esta configuração para desativar a pré-renderização estática
-  experimental: {
-    // Isso fará com que as páginas sejam renderizadas no momento da solicitação
-    appDir: true,
-  },
-  // Configurar páginas específicas para serem renderizadas no lado do cliente
-  unstable_runtimeJS: true,
 }
 
 export default nextConfig
